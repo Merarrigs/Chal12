0f66dfe4e5ca234211d62cf8c5bca0c1f2d24260
Merarrigs <merarri.007@outlook.com>
2025-01-26 21:47:55 -0600

    index.css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,6 @@ function Footer() {
         <Link to="https://github.com/Merarrigs">
           <img src="https://cdn-icons-png.flaticon.com/256/25/25231.png" />
         </Link>
-  
-        <Link to="https://www.linkedin.com/in/jmerarrigs/">
-          <img src={linkedInIcon} />
-        </Link>
       </div>
     )
   }
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,19 +2,20 @@ import React from 'react';
 
 
     <div>
-       <h3>Front end Technologies</h3>
+       <h3>Front end</h3>
        <ul>
+          <li>React</li>
           <li>HTML5</li>
           <li>CSS3</li>
-          <li>React</li>
-          <li>Sass</li>
-          <li>Angular</li>
+          
+    
         </ul>
-        <h3>Back end Technologies</h3>
+        <h3>Back end</h3>
         <ul>
           <li>Nodejs</li>
-          <li>API management</li>
           <li>SQL</li>
+          
+          
         </ul>
     </div>
 
